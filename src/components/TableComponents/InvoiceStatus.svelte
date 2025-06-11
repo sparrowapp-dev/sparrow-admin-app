5fb8288b8e6f57c3143bb5835d9beb903720b7a4
Abhishek <abhishek.pub9503@gmail.com>
2025-06-11 17:46:19 +0530

    feat: Added invoice page with api

---
+++ src/components/TableComponents/InvoiceStatus.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Clock from '@/assets/icons/Clock.svelte';
+  import Success from '@/assets/icons/Success.svelte';
+
+  export let status: string;
+</script>
+
+<span class="flex items-center gap-1">
+  {#if status === 'upcoming'}
+    <Clock />
+    <span class="text-fs-ds-12 text-neutral-50">Upcoming</span>
+  {:else if status === 'open'}
+    <Success />
+    <span class="text-fs-ds-12 text-neutral-50">Success</span>
+  {:else}
+    <span class="text-fs-ds-12 text-neutral-400 capitalize">-</span>
+  {/if}
+</span>
