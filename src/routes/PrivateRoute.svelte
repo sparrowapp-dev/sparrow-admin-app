9c79e1264e14de519fdd21f788be59808b0358ff
mithesh54 <mithesh.bangera@techdome.net.in>
2025-06-24 12:18:54 +0530

    fix: login

--- src/routes/PrivateRoute.svelte
+++ src/routes/PrivateRoute.svelte
@@ -10,20 +10,6 @@
 
   // Check if user is logged in
   let isLoggedIn: boolean = !!$auth?.token;
-
-  // Add handler for authentication changes
-  onMount(() => {
-    // Check token validity immediately when route is accessed
-    if (isLoggedIn) {
-      checkAndRefreshToken().catch(() => {
-        // If token refresh fails, redirect to login
-        navigate('/login');
-      });
-    } else if (!isLoggedIn) {
-      // If not logged in, redirect immediately
-      navigate('/login');
-    }
-  });
 </script>
 
 {#if isLoggedIn}
